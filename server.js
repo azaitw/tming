6dbbfd5f81d5fdb10e4dde1607ea24a27170497c
Azai <mightybj@gmail.com>
2015-03-10 23:36:04 +0800

    Add simple node server for verification

---
+++ server.js
@@ -0,0 +1,14 @@
+/* jshint strict:true */
+
+'use strict';
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/output/index.html');
+});
+app.set('port', port);
+app.listen(app.get('port'), function () {
+    console.log('Server started at port ' + port);
+});
